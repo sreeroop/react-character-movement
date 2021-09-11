0207abf8b15e8639782b28699a55bbf476b6b031
sreeroop <sreeroopsk10@gmail.com>
2021-09-12 01:31:26 +0530

    joystick added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import logo from './logo.svg';
+
 import { useEffect } from 'react';
 import './App.css';
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-
+import joystickblue from './assets/joystickblue.png'
+import joystickbase from './assets/joystickbase.png'
 
 function App() {
   let _APP = null;
   useEffect(() => {
     _APP = new CharacterControllerDemo();
+    // localStorage.setItem('_APP', new CharacterControllerDemo());
+    // localStorage.getItem('_APP')
   }, [])
 
   class BasicCharacterControllerProxy {
@@ -110,7 +113,7 @@ function App() {
         acc.multiplyScalar(2.0);
       }
 
-      if (this._stateMachine._currentState.Name == 'dance') {
+      if (this._stateMachine._currentState.Name === 'dance') {
         acc.multiplyScalar(0.0);
       }
 
@@ -157,7 +160,103 @@ function App() {
       }
     }
   };
+  class JoystickController {
+    constructor(stickID, maxDistance, deadzone) {
+      this.id = stickID;
+      let stick = document.getElementById(stickID);
 
+      // location from which drag begins, used to calculate offsets
+      this.dragStart = null;
+
+      // track touch identifier in case multiple joysticks present
+      this.touchId = null;
+
+      this.active = false;
+      this.value = { x: 0, y: 0 };
+
+      let self = this;
+
+      function handleDown(event) {
+        self.active = true;
+
+        // all drag movements are instantaneous
+        stick.style.transition = '0s';
+
+        // touch event fired before mouse event; prevent redundant mouse event from firing
+        event.preventDefault();
+
+        if (event.changedTouches)
+          self.dragStart = { x: event.changedTouches[0].clientX, y: event.changedTouches[0].clientY };
+        else
+          self.dragStart = { x: event.clientX, y: event.clientY };
+
+        // if this is a touch event, keep track of which one
+        if (event.changedTouches)
+          self.touchId = event.changedTouches[0].identifier;
+      }
+
+      function handleMove(event) {
+        if (!self.active) return;
+
+        // if this is a touch event, make sure it is the right one
+        // also handle multiple simultaneous touchmove events
+        let touchmoveId = null;
+        if (event.changedTouches) {
+          for (let i = 0; i < event.changedTouches.length; i++) {
+            if (self.touchId === event.changedTouches[i].identifier) {
+              touchmoveId = i;
+              event.clientX = event.changedTouches[i].clientX;
+              event.clientY = event.changedTouches[i].clientY;
+            }
+          }
+
+          if (touchmoveId == null) return;
+        }
+
+        const xDiff = event.clientX - self.dragStart.x;
+        const yDiff = event.clientY - self.dragStart.y;
+        const angle = Math.atan2(yDiff, xDiff);
+        const distance = Math.min(maxDistance, Math.hypot(xDiff, yDiff));
+        const xPosition = distance * Math.cos(angle);
+        const yPosition = distance * Math.sin(angle);
+
+        // move stick image to new position
+        stick.style.transform = `translate3d(${xPosition}px, ${yPosition}px, 0px)`;
+
+        // deadzone adjustment
+        const distance2 = (distance < deadzone) ? 0 : maxDistance / (maxDistance - deadzone) * (distance - deadzone);
+        const xPosition2 = distance2 * Math.cos(angle);
+        const yPosition2 = distance2 * Math.sin(angle);
+        const xPercent = parseFloat((xPosition2 / maxDistance).toFixed(4));
+        const yPercent = parseFloat((yPosition2 / maxDistance).toFixed(4));
+
+        self.value = { x: xPercent, y: yPercent };
+      }
+
+      function handleUp(event) {
+        if (!self.active) return;
+
+        // if this is a touch event, make sure it is the right one
+        if (event.changedTouches && self.touchId !== event.changedTouches[0].identifier) return;
+
+        // transition the joystick position back to center
+        stick.style.transition = '.2s';
+        stick.style.transform = `translate3d(0px, 0px, 0px)`;
+
+        // reset everything
+        self.value = { x: 0, y: 0 };
+        self.touchId = null;
+        self.active = false;
+      }
+
+      stick.addEventListener('mousedown', handleDown);
+      stick.addEventListener('touchstart', handleDown);
+      document.addEventListener('mousemove', handleMove, { passive: false });
+      document.addEventListener('touchmove', handleMove, { passive: false });
+      document.addEventListener('mouseup', handleUp);
+      document.addEventListener('touchend', handleUp);
+    }
+  }
   class BasicCharacterControllerInput {
     constructor() {
       this._Init();
@@ -172,9 +271,44 @@ function App() {
         space: false,
         shift: false,
       };
+      this.myStick = new JoystickController("stick", 64, 8);
+
+      let joystick = document.getElementById("joystick")
+
+
+      document.addEventListener("mouseover", (e) => this._joyOn(this.myStick), false);
+      document.addEventListener("touchend", (e) => this._joyOff(), false);
+      document.addEventListener("mouseleave", (e) => this._joyOff(), false);
+      document.addEventListener("touchmove", (e) => this._joyOn(this.myStick), false);
       document.addEventListener('keydown', (e) => this._onKeyDown(e), false);
       document.addEventListener('keyup', (e) => this._onKeyUp(e), false);
 
+    }
+
+    _joyOn = (myStick) => {
+      let x = this.myStick.value.x;
+      let y = this.myStick.value.y;
+      console.log(x, ",", y);
+      if (x > .708) {
+        this._keys.left = true;
+      } else if (x < -.708) {
+        this._keys.right = true;
+      } else if (y > .708) {
+        this._keys.backward = true;
+      } else if (y < -0.708) {
+        this._keys.forward = true;
+      } else {
+        this._keys.backward = false;
+        this._keys.forward = false;
+        this._keys.right = false;
+        this._keys.left = false;
+      }
+    }
+    _joyOff = () => {
+      this._keys.backward = false;
+      this._keys.forward = false;
+      this._keys.right = false;
+      this._keys.left = false;
     }
     _onKeyDown(event) {
       switch (event.keyCode) {
@@ -651,6 +785,14 @@ function App() {
 
   return (
     <div className="App">
+      <div id="joystick-container">
+        <div id="joystick">
+          <img src={joystickbase} />
+          <div id="stick">
+            <img src={joystickblue} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
